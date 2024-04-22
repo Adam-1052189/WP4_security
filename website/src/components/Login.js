911d3c351c244bfc21a49b142cd3cfba604355ae
Olcay <1018693@hr.nl>
2024-04-22 15:51:18 +0200

    - Login pagina toegeveogd aan website

--- website/src/components/Login.js
+++ website/src/components/Login.js
@@ -4,31 +4,28 @@ function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = async (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        const response = await fetch('http://localhost:8000/api-auth/login/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username, password })
-        });
-        const data = await response.json();
-        console.log(data);
+        console.log(`Inloggen met gebruikersnaam: ${username} en wachtwoord: ${password}`);
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username:
-                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-            </label>
-            <label>
-                Password:
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </label>
-            <button type="submit">Login</button>
-        </form>
+        <div>
+            <h2>Inloggen</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Gebruikersnaam:
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                    Wachtwoord:
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </label>
+                <br />
+                <button type="submit">Inloggen</button>
+            </form>
+        </div>
     );
 }
 
