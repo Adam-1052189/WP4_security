f0a85de2da763afdc5fda18332737c738b7b0dd8
Olcay <1018693@hr.nl>
2024-04-25 14:45:59 +0200

    - inlog pagina aangepast

--- react/App.js
+++ react/App.js
@@ -1,49 +1,19 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import DocentDashboardMobile from "./src/screens/DocentDashboardMobile";
-
-function LoginScreen({ navigation }) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleSubmit = () => {
-        // Voer hier de inloglogica uit
-        console.log(`Inloggen met gebruikersnaam: ${username} en wachtwoord: ${password}`);
-    };
-
-    return (
-        <View>
-            <Text>Inloggen</Text>
-            <Text>Gebruikersnaam:</Text>
-            <TextInput value={username} onChangeText={setUsername} />
-            <Text>Wachtwoord:</Text>
-            <TextInput value={password} onChangeText={setPassword} secureTextEntry />
-            <Button title="Inloggen" onPress={handleSubmit} />
-        </View>
-    );
-}
+import Login from './components/Login';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="DocentDashboard" component={DocentDashboardMobile} />
-        {/* Andere schermen na login */}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+      <NavigationContainer>
+          <Stack.Navigator initialRouteName="">
+              <Stack.Screen name="Login" component={Login}/>
+              <Stack.Screen name="DocentDashboard" component={DocentDashboardMobile}/>
+              {/* Andere schermen na login */}
+          </Stack.Navigator>
+      </NavigationContainer>
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- react/components/Login.js
+++ react/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Text, TouchableOpacity, Linking } from 'react-native';
 
 function Login() {
   const [username, setUsername] = useState('');
@@ -19,20 +19,29 @@ function Login() {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.loginText}>Inloggen</Text>
       <TextInput
         style={styles.input}
-        placeholder="Username"
+        placeholder="Gebruikersnaam"
         onChangeText={setUsername}
         value={username}
       />
       <TextInput
         style={styles.input}
-        placeholder="Password"
+        placeholder="Wachtwoord"
         secureTextEntry
         onChangeText={setPassword}
         value={password}
       />
       <Button title="Login" onPress={handleSubmit} />
+      <View style={styles.footer}>
+        <TouchableOpacity onPress={() => Linking.openURL('tel:+310107941111')}>
+          <Text style={styles.footerText}>010 - 794 1111</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Linking.openURL('mailto:service@hr.nl')}>
+          <Text style={styles.footerText}>service@hr.nl</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -41,13 +50,48 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    padding: 10
+    padding: 0,
+    backgroundColor: '#fff7ea',
+  },
+  loginText: {
+    color: '#001e48',
+    fontSize: 24,
+    fontFamily: 'Poppins',
+    textAlign: 'center',
+    marginBottom: 20,
+    marginTop: 50,
   },
   input: {
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
+  },
+  loginButton: {
+    height: 50,
+    backgroundColor: '#d30f4c',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 12,
+    borderRadius: 5,
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontFamily: 'Poppins',
+  },
+  footer: {
+    backgroundColor: '#d30f4c',
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  footerText: {
+    color: '#fff',
+    textDecorationLine: 'underline',
   },
 });
 
