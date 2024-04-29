788f20526a84046250162e4c2da3d4715e9de929
Adam-1052189 <1052189@hr.nl>
2024-04-29 14:27:55 +0200

    Logo toegevoegd, begin aan header gemaakt.

--- react/components/Header.js
+++ react/components/Header.js
@@ -1,8 +1,21 @@
 import React from "react"
+import "./CSS/header_stylesheet"
+import "./Images/Glitch_logo.png"
 
 function Header() {
     return (
-        <header>This is the header</header>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/meldingen">meldingen</a></li>
+                    <li><a href="/profiel">profiel</a></li>
+                </ul>
+            </nav>
+        </header>
+
     )
 }
 
