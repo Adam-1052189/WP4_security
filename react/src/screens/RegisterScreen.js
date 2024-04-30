b00f7c6c2c103a146785afcb8b73a89e9f1b6932
Omer <1080314@hr.nl>
2024-04-30 14:15:37 +0200

    - onSubmitEditing toegevoegd, zorgt ervoor dat er op de knop registeren wordt gedrukt met een enter

--- react/src/screens/RegisterScreen.js
+++ react/src/screens/RegisterScreen.js
@@ -48,6 +48,7 @@ function RegisterScreen() {
         placeholder="Studentnummer of personeelscode"
         onChangeText={setEmail}
         value={email}
+        onSubmitEditing={handleSubmit}
       />
       <TextInput
         style={styles.input}
@@ -55,6 +56,7 @@ function RegisterScreen() {
         secureTextEntry
         onChangeText={setPassword}
         value={password}
+        onSubmitEditing={handleSubmit}
       />
       <Button
           title="Registreer" onPress={handleSubmit}
