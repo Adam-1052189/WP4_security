b706c0801962020dac21440b8b2d83af7a6d1a8f
Adam-1052189 <1052189@hr.nl>
2024-06-05 13:33:45 +0200

    header en footer verwijderd

--- react/src/screens/StudentDashboard.js
+++ react/src/screens/StudentDashboard.js
@@ -1,21 +1,19 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+
 import Domeinen from "../components/Domeinen";
 
 function StudentDashboard() {
     return (
+
         <View>
-            <Header />
-                <View>
-                    <Text>hier komt te staan welkom naam gebruiker</Text>
-                    <Text>hier staan de vakken</Text>
-                    <Domeinen />
-                </View>
-            <Footer />
+            <Domeinen />
+            <Text>hier komt te staan welkom naam gebruiker</Text>
+            <Text>hier staan de vakken</Text>
+            
         </View>
+
     )
 }
 
