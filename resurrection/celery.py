3e71e5358bd50f264718862b61482df71b7e27ca
Adam-1052189 <1052189@hr.nl>
2024-06-27 20:47:36 +0200

    C:\Python312\python.exe -m celery -A resurrection worker --loglevel=info  commando foutmelding fix door aanpassing celery.py. maakt geen verbinding met de server

--- resurrection/celery.py
+++ resurrection/celery.py
@@ -1,14 +1,16 @@
+import os
 from celery import Celery
 from celery.schedules import crontab
 
-app = Celery('resurrection')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resurrection.settings')
 
+app = Celery('resurrection')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
     'check-deadlines-every-minute': {
         'task': 'glitch.tasks.check_deadlines',
-        'schedule': crontab(minute='*'), 
+        'schedule': crontab(minute='*'),
     },
 }
