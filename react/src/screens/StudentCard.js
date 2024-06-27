60e08c0ef00f0fdfb54aa2be9575ae6c21b9d746
Omer <1080314@hr.nl>
2024-06-27 17:23:00 +0200

    - BUGFIX

--- react/src/screens/StudentCard.js
+++ react/src/screens/StudentCard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, Modal } from 'react-native';
+import {View, Text, StyleSheet, FlatList, TouchableOpacity, TextInput} from 'react-native';
 
 const StudentCard = ({ studentId, closeModal }) => {
     const [activiteiten, setActiviteiten] = useState([]);
